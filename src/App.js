f6c1309d515b2d23dcd03e9c3a94ae10b79bab7f
Mike Manchee <mikemanchee@gmail.com>
2021-02-02 11:40:04 -0800

    add Typographys with material-ui

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ import { orange, green } from '@material-ui/core/colors';
 import Checkboxes from './components/Checkboxes';
 import Buttons from './components/Buttons';
 import TextFields from './components/TextFields';
+import Typographies from "./components/Typographies";
 
 const theme = createMuiTheme({
+  typography: {
+    h2: {
+      fontSize: 32,
+      marginBottom: 15,
+    }
+  },
   palette: {
     primary: {
       main: orange[400]
@@ -22,6 +29,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
+        <Typographies />
         <Checkboxes />
         <Buttons />
         <TextFields />
---
+++ src/components/Typographies.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Typography } from '@material-ui/core';
+
+const Typographies = () => {
+  return (
+    <div>
+      <Typography variant='h1' component='div'>
+        Welcome to MUI
+      </Typography>
+      <Typography variant='h3'>
+        This is MUI
+      </Typography>
+      <Typography variant='subtitle1'>
+        Learn to use MUI
+      </Typography>
+      <Typography variant='body1'>
+        test body1
+      </Typography>
+
+    </div>
+  )
+}
+
+export default Typographies
